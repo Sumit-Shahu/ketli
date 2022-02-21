e49689bf3768f92ee0f030a0dff9571381f8f8f8
Your Name <you@example.com>
2022-02-21 23:30:17 +0530

    My first project commit

---
+++ gitproject/src/main/java/gitproject/gitproject/App.java
@@ -0,0 +1,17 @@
+package gitproject.gitproject;
+
+public class App {
+	
+    public static void main(String[] args){
+    	new App().start();
+    }
+    
+    public App() {
+    	
+	}
+    
+    public void start() {
+
+	}
+    
+}
